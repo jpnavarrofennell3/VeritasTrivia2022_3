87df56f229fef7df70eeb9bc9c7bae04d404b373
JuanNavarro <106715486+jpnavarrofennell3@users.noreply.github.com>
2022-10-18 21:42:28 -0600

    Preguntas de ejemplo

--- LogicaJuego/Juego.cs
+++ LogicaJuego/Juego.cs
@@ -40,8 +40,33 @@ namespace LogicaJuego
         public void FinJuego()
         { }
 
-        public void CargarDatos() 
-        { 
+        public void CargarDatos()
+        {
+            // Nivel muy fácil
+            bancoDePreguntas[0].nombreDeLaDificultad = "Muy Fácil";
+
+            bancoDePreguntas[0].preguntas[0].enunciadoDeLaPregunta = "¿Dónde se ubica Costa Rica?";
+            bancoDePreguntas[0].preguntas[0].respuestaCorrecta = 1;
+            bancoDePreguntas[0].preguntas[0].respuestasDeLaPregunta[0].EnunciadoDeLaRespuesta = "Asia";
+            bancoDePreguntas[0].preguntas[0].respuestasDeLaPregunta[1].EnunciadoDeLaRespuesta = "America";
+            bancoDePreguntas[0].preguntas[0].respuestasDeLaPregunta[2].EnunciadoDeLaRespuesta = "Europa";
+            bancoDePreguntas[0].preguntas[0].respuestasDeLaPregunta[3].EnunciadoDeLaRespuesta = "Oceanía";
+
+            bancoDePreguntas[0].preguntas[1].enunciadoDeLaPregunta = "¿Dónde se ubica Japón?";
+            bancoDePreguntas[0].preguntas[1].respuestaCorrecta = 3;
+            bancoDePreguntas[0].preguntas[1].respuestasDeLaPregunta[0].EnunciadoDeLaRespuesta = "Asia";
+            bancoDePreguntas[0].preguntas[1].respuestasDeLaPregunta[1].EnunciadoDeLaRespuesta = "America";
+            bancoDePreguntas[0].preguntas[1].respuestasDeLaPregunta[2].EnunciadoDeLaRespuesta = "Europa";
+            bancoDePreguntas[0].preguntas[1].respuestasDeLaPregunta[3].EnunciadoDeLaRespuesta = "Oceanía";
+
+            bancoDePreguntas[0].preguntas[2].enunciadoDeLaPregunta = "";
+            bancoDePreguntas[0].preguntas[3].enunciadoDeLaPregunta = "";
+            bancoDePreguntas[0].preguntas[4].enunciadoDeLaPregunta = "";
+
+            bancoDePreguntas[1].nombreDeLaDificultad = "Fácil";
+            bancoDePreguntas[2].nombreDeLaDificultad = "Normal";
+            bancoDePreguntas[3].nombreDeLaDificultad = "Difícil";
+            bancoDePreguntas[4].nombreDeLaDificultad = "Muy Difícil";
         }
     }
 }
